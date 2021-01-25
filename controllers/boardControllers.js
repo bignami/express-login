23aef3a8829d4ea4c1203506d42144809e32c47b
bignami <bignamipk@gmail.com>
2021-01-25 15:27:16 +0900

    board update, delete Add

--- controllers/boardControllers.js
+++ controllers/boardControllers.js
@@ -28,3 +28,56 @@ exports.makeBoardPost = (req, res) => {
     res.redirect('/board');
     
 }
+exports.detailBoardGet = async (req,res) => {
+   
+    await boardModel.find({'_id' : req.params.id }, (err, docs) => {
+        if(docs.length>0) {
+            
+            res.render('./board/boardDetail',{docs});
+        }
+        else {
+            res.status(404).send('잘못된 요청접근입니다.!');
+        }
+    } )
+}
+
+exports.detailBoardEditGet = async (req,res) => {
+    
+    await boardModel.find({'_id': req.params.id}, (err, docs) => {
+        res.render('./board/boardDetailEdit',{docs});
+    })
+    
+}
+    
+
+exports.detailBoardPost = async (req,res) => {
+    
+    console.log(req.params.id);
+
+
+    boardModel.findByIdAndUpdate(req.params.id, {service : req.body.user_service,
+        subject : req.body.user_title,
+        content: req.body.user_content,
+        auth_key: req.body.user_authKey,
+        price : req.body.user_price}, (err,result) => {
+            if(err) {
+                console.error('에러발생');
+            } else {
+                console.log('정상변경',result);
+                res.redirect('/board/detail/' +req.params.id);
+            }
+        })
+}
+
+exports.deleteDetailBoard = (req, res) => {
+    console.log(req.params.id);
+
+    boardModel.findByIdAndDelete(req.params.id, (err,result) => {
+        if(err) {
+            console.error('에러발생');
+        } else {
+            console.log('정상삭제');
+            res.redirect('/board');
+        }
+    })
+}
--- routers/board/board.js
+++ routers/board/board.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const {boardGet} = require('../../controllers/boardControllers');
+const {boardGet, detailBoardGet, detailBoardEditGet, detailBoardPost,deleteDetailBoard } = require('../../controllers/boardControllers');
 
 router.get('/', boardGet);
 router.get('/');
+router.get('/detail/:id',detailBoardGet);
+router.get('/detail/:id/edit',detailBoardEditGet);
+router.post('/detail/delete/:id',deleteDetailBoard );
+router.post('/detail/:id',detailBoardPost);
 
 module.exports = router;
